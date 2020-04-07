c686476cb76ced9957fb08384a0603dfccace8cc
Conor Walsh <conorwalsh0703@gmail.com>
2020-04-07 12:45:07 +0100

    setup Express server and MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const connectDB = require('./config/db')
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+const auth = require('./routes/api/auth')
 
 const app = express()
 
+// Connect Databade
+connectDB()
+
 app.get('/', (req, res) => res.send('API Running'))
+
+// Define Routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
+app.use('/api/auth', auth)
 
 const PORT = process.env.PORT || 5000
 
