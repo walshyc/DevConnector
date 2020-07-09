cac91f801757ae98bc778ea4323af301533f5897
Conor Walsh <conorwalsh0703@gmail.com>
2020-07-09 21:16:19 +0100

    setup for deployment on Heroku

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
-const express = require('express')
-const connectDB = require('./config/db')
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
-const auth = require('./routes/api/auth')
+const express = require("express");
+const connectDB = require("./config/db");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const auth = require("./routes/api/auth");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-// Connect Databade
-connectDB()
+// Connect Database
+connectDB();
 
 // Init Middleware
-app.use(express.json({
-    extended: false
-}))
-
-app.get('/', (req, res) => res.send('<h1>API Running</h1>'))
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 // Define Routes
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
-app.use('/api/auth', auth)
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+app.use("/api/auth", auth);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Define port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
